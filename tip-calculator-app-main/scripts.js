32462efbdd9e616b8e4ba9ad0686b0450debcd91
Anurag Saraswat <anu01saraswat@gmail.com>
2021-08-29 22:53:12 +0530

    Almost complete Mobile Layout and JS

---
+++ tip-calculator-app-main/scripts.js
@@ -0,0 +1,31 @@
+var tip;
+
+function buttonCheck(current) {
+    var activeTip = document.getElementsByClassName("active");
+    if(activeTip.length != 0) {
+        activeTip[0].classList.remove("active");
+    }
+    if(current.id == "custom") {
+        tip = parseFloat(current.value);
+    } else {
+        tip = parseFloat(current.id.substring(4));
+    }
+    current.classList.add("active");
+}
+
+function calculate() {
+    var bill = parseFloat(document.calculator.bill.value);
+    var people = parseFloat(document.calculator.people.value);
+    var tipAmount = (tip * bill) / (100 * people);
+    var totalAmount = (bill + ((bill * tip) / 100)) / people;
+    document.getElementById("tip-value").innerHTML = "$" + tipAmount;
+    document.getElementById("total-value").innerHTML = "$" + totalAmount;
+    document.getElementsByClassName("reset-button")[0].classList.remove("inactive");
+}
+
+function resetCalculator() {
+    console.log("working");
+    document.getElementsByClassName("reset-button")[0].classList.add("inactive");
+    document.getElementById("tip-value").innerHTML = "$0.00";
+    document.getElementById("total-value").innerHTML = "$0.00";
+}
