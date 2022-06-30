6b0c878c27c2dc3d36cfc9be4095b1bd3bc7519f
Anurag Saraswat <anu01saraswat@gmail.com>
2022-06-30 15:02:48 +0530

    Revert deploy trial

--- calculator-app-main/calculator/vue.config.js
+++ calculator-app-main/calculator/vue.config.js
@@ -1,8 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "front-end-mentor.github.io/calculator-app-main/calculator/dist/"
-      : "/",
 });
