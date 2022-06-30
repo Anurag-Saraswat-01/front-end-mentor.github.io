21407babd6c861d9063ce793025bdd29b4617350
Anurag Saraswat <anu01saraswat@gmail.com>
2022-06-30 15:00:06 +0530

    deploy trial v1.1

--- calculator-app-main/calculator/vue.config.js
+++ calculator-app-main/calculator/vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "front-end-mentor.github.io/calculator-app-main/calculator/dist/"
+      : "/",
+});
